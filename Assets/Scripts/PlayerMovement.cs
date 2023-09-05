1e74eb0a5c338bec7b28c7701a1464a6dd1a7769
Willeboy175 <william.strandberg@elev.ga.lbs.se>
2023-09-05 11:10:59 +0200

    Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float playerSpeed = 1;
+    Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+
+        rb.velocity = playerSpeed * Time.deltaTime * movement.normalized;
+    }
+}
