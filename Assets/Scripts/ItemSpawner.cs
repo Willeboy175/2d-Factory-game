0fc864d6211aaa4c48ffa352e6caa071e7ee5d9c
Willeboy175 <william.strandberg@elev.ga.lbs.se>
2023-09-07 11:29:29 +0200

    Item Spawner

---
+++ Assets/Scripts/ItemSpawner.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour
+{
+    public int direction = 1;
+    public Rigidbody2D item;
+    public Transform spawnPos;
+    public float cooldown = 1;
+    public float timer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > cooldown)
+        {
+            Instantiate(item, spawnPos);
+            timer = 0;
+        }
+    }
+}
