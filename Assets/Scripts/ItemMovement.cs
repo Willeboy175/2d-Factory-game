407508da43527de00ca2b9a5c68bdaf0048cfb5d
Willeboy175 <william.strandberg@elev.ga.lbs.se>
2023-09-25 21:58:37 +0200

    WORKING CONVEYORS!!!!

--- Assets/Scripts/ItemMovement.cs
+++ Assets/Scripts/ItemMovement.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class ItemMovement : MonoBehaviour
 {
     public Rigidbody2D item;
+    public Transform raycastOrigin;
+    public float cooldown = 1;
+    public float timer = 0;
+    public LayerMask layerMaskConveyor;
+    public LayerMask layerMaskItem;
+
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +21,24 @@ public class ItemMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+
+        RaycastHit2D getRotation = Physics2D.Raycast(item.position, item.transform.right, 0.4f, layerMaskConveyor);
+        if (timer > cooldown && getRotation.transform != null)
+        {
+            item.transform.rotation = getRotation.transform.rotation;
+
+            RaycastHit2D hitItem = Physics2D.Raycast(raycastOrigin.position, item.transform.right, 0.2f, layerMaskItem);
+            if (hitItem.transform == null)
+            {
+                RaycastHit2D hitConveyor = Physics2D.Raycast(raycastOrigin.position, item.transform.right, 0.4f, layerMaskConveyor);
+                if (hitConveyor.transform != null)
+                {
+                    item.transform.position = hitConveyor.transform.position;
+                    timer = 0;
+                }
+            }
+            
+        }
     }
 }
--- Assets/Scripts/ItemSpawner.cs
+++ Assets/Scripts/ItemSpawner.cs
@@ -1,25 +1,22 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemSpawner : MonoBehaviour
 {
-    public int direction = 1;
     public Rigidbody2D item;
-    public Transform raycast;
     public Transform spawnPos;
+    public Transform raycastOrigin;
     public float cooldown = 1;
     public float timer = 0;
-
-    int layerMask;
+    public LayerMask layerMaskConveyor;
+    public LayerMask layerMaskItem;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        raycast = GetComponent<Transform>();
-        layerMask = ~(LayerMask.GetMask("Conveyor"));
+        raycastOrigin = GetComponent<Transform>();
     }
 
     // Update is called once per frame
@@ -27,21 +24,16 @@ public class ItemSpawner : MonoBehaviour
     {
         timer += Time.deltaTime;
 
-        RaycastHit2D hit = Physics2D.Raycast(raycast.transform.position, new Vector2(1, 0), 0.5f, layerMask);
-
-        if (timer > cooldown && hit.transform.gameObject != null)
+        RaycastHit2D hitItem = Physics2D.Raycast(raycastOrigin.position, raycastOrigin.right, 1, layerMaskItem);
+        if (timer > cooldown && hitItem.transform == null)
         {
-            print(hit.transform.name);
-            if (hit.transform.CompareTag("Conveyor") == true)
+            RaycastHit2D hitConveyor = Physics2D.Raycast(raycastOrigin.position, raycastOrigin.right, 1, layerMaskConveyor);
+            if (hitConveyor.transform != null)
             {
-                print("Conveyor");
-                Instantiate(item, spawnPos.transform.position, raycast.transform.rotation);
+                Instantiate(item, spawnPos.position, raycastOrigin.rotation);
                 timer = 0;
             }
-        }
-        else if (timer > cooldown * 2)
-        {
-            timer = 1;
+            
         }
     }
 }
